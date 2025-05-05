1b165d7baba6d526267682079dc1910efb3fde3c
matheus de souza <mtssantos101@gmail.com>
2025-05-04 21:52:30 -0300

    adiciona a função menu na main

--- main.c
+++ main.c
@@ -1,56 +1,6 @@
-#include "filme.h"
+#include "menu.h"
 
 int main(int argc, char *argv[]){
-    PListaFilmes lista = NULL;
-    int opcao, id;
-    
-    do {
-        printf("Menu:\n");
-        printf("1. Cadastrar Filme\n");
-        printf("2. Mostrar Filmes\n");
-        printf("3. Buscar Filme\n");
-        printf("4. Excluir Filme\n");
-        printf("5. Sair\n");
-        printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-        
-        switch(opcao) {
-            case 1:
-                cadastrarFilme(&lista);
-                break;
-            case 2:
-                if (lista == NULL) {
-                    printf("Nenhum filme cadastrado.\n");
-                    break;
-                }
-                printf("Lista de Filmes:\n");
-                mostrarFilmes(lista);
-                break;
-            case 3: {
-                printf("Digite o ID do filme a ser buscado: ");
-                scanf("%d", &id);
-                PListaFilmes filmeEncontrado = buscarFilme(lista, id);
-                if (filmeEncontrado != NULL) {
-                    printf("Filme encontrado: ID: %d, Nome: %s, Duracao: %d minutos\n", filmeEncontrado->filme.id, filmeEncontrado->filme.nomeFilme, filmeEncontrado->filme.duracaoMin);
-                } else {
-                    printf("Filme com ID %d nao encontrado.\n", id);
-                }
-                break;
-            }
-            case 4:
-		printf("Digite o ID do filme a ser excluido: \n");
-		scanf("%d", &id);
-		excluirFilme(&lista, id);
-		break;    
-                
-            case 5:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opcao invalida! Tente novamente.\n");
-        }
-    } while(opcao != 5);
-    
+    menu();
     return 0;
-
-};
+}
